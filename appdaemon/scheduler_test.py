cf2eec5c4d1a85879f2b17f4fe96c06ebb26f7bc
Matthias Uschok <dev@uschok.de>
2018-02-15 18:59:16 +0100

    Let the test app dump the schedule multiple times a day for debugging purposes

--- appdaemon/scheduler_test.py
+++ appdaemon/scheduler_test.py
@@ -1,20 +1,35 @@
 import appdaemon.appapi as appapi
-from datetime import datetime
+import appdaemon.conf as conf
+import datetime
+import time
 
 class SchedulerTest(appapi.AppDaemon):
     def initialize(self):
-        # self.run_every(self.scheduler_function, datetime(2018, 2, 3, 13, 28), 60, foo="bar")
-        schedule = self.get_scheduler_entries()
-        for name, entries in schedule.items():
-            self.log("Showing entries of {}".format(name))
-            for entry, entrydata in entries.items():
-                self.log("Showing entrydata of {}".format(entry))
-                for key, datum in entrydata.items():
-                    if key == 'offset' or key == 'timestamp' or key == 'interval' or key == 'repeat':
-                        self.log("{} => {}".format(key, datetime.fromtimestamp(datum)))
-                    else:
-                        self.log("{} => {}".format(key, datum))
+        self.run_daily(self.dump_schedule, datetime.time(6,5))
+        self.run_daily(self.dump_schedule, datetime.time(6,35))
+        self.run_daily(self.dump_schedule, datetime.time(7,35))
+        self.run_daily(self.dump_schedule, datetime.time(8,5))
+        self.run_daily(self.dump_schedule, datetime.time(16,5))
+        self.run_daily(self.dump_schedule, datetime.time(22,5))
+        self.run_daily(self.dump_schedule, datetime.time(0,5))
         self.log("Scheduler test initialized")
 
-    def scheduler_function(self, kwargs):
-        self.log("Running scheduled task with args {}".format(kwargs))
+    def dump_schedule(self, kwargs):
+        self.log("--------------------------------------------------")
+        self.log("Scheduler Table")
+        self.log("--------------------------------------------------")
+        for name in conf.schedule.keys():
+            self.log("{}:".format(name))
+            for entry in sorted(
+                    conf.schedule[name].keys(),
+                    key=lambda uuid_: conf.schedule[name][uuid_]["timestamp"]
+            ):
+                self.log(
+                    "  Timestamp: {} - data: {}".format(
+                        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(
+                            conf.schedule[name][entry]["timestamp"]
+                        )),
+                        conf.schedule[name][entry]
+                    )
+                )
+        self.log("--------------------------------------------------")
